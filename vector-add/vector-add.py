98d24808d89050fc2c07790a59fcd13bd6d7da46
linuxlonelyeagle <2020382038@qq.com>
2024-07-09 07:16:48 +0000

    add vector-add.

---
+++ vector-add/vector-add.py
@@ -0,0 +1,43 @@
+import torch
+import triton
+import triton.language as tl
+import triton.compiler as compiler
+
+@triton.jit
+def add_kernel(x_ptr,
+               y_ptr,
+               output_ptr,
+               n_elements,
+               BLOCK_SIZE: tl.constexpr,
+               ):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+def add(x: torch.Tensor, y: torch.Tensor):
+    output = torch.empty_like(x)
+    assert x.is_cuda and y.is_cuda and output.is_cuda
+    n_elements = output.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    compiled_kernel : compiler.CompiledKernel = add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+    print("type:", compiled_kernel)
+    # convert kernel to MLIR
+    module = compiled_kernel.asm['ttir']
+    return output
+
+torch.manual_seed(0)
+size = 98432
+x = torch.rand(size, device='cuda')
+y = torch.rand(size, device='cuda')
+output_torch = x + y
+output_triton = add(x, y)
+print(output_torch)
+print(output_triton)
+print(f'The maximum difference between torch and triton is '
+      f'{torch.max(torch.abs(output_torch - output_triton))}')
+
